b0dd90401f174a54eeb4a064d572db440eee53d8
Rahul Sharma <90137rahul@gmail.com>
2020-10-02 00:56:53 +0530

    Destructuring Concept added

--- forecast.js
+++ forecast.js
@@ -4,14 +4,14 @@
       const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + encodeURIComponent(latitude) + '&lon=' + encodeURIComponent(longitude) + '&appid=7e14c86b6631b53d4d360bfd6bd19595&units=metric'
           //console.log(latitude, longitude)
           //console.log(url);
-      request({ url: url, json: true }, (error, response) => {
+      request({ url, json: true }, (error, { body }) => {
 
           if (error) {
               callback('unable to connect to weather services!', undefined)
-          } else if (response.body.error) {
+          } else if (body.error) {
               callback('Unable to find Location', undefined)
           } else {
-              callback(undefined, 'It is currently ' + response.body.main.temp + ' degree celsius with humdity ' + response.body.main.humidity)
+              callback(undefined, 'It is currently ' + body.main.temp + ' degree celsius with humdity ' + body.main.humidity)
           }
       })
 
